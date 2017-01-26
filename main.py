f3e94818d45e8e9f167c8fe8469adc5711d86d8d
Nicholas Martellaro <Nicholas Martellaro>
2017-01-25 20:59:33 -0600

    completed web caesar

--- main.py
+++ main.py
@@ -16,20 +16,40 @@
 #
 import webapp2
 import ceasar
+import cgi
 
 
+def build_page(textarea_content):
 
+        rot_label = "<label>Rotate by :</label>"
+        message_label = "<label>Type a message:</label>"
+        textarea = "<textarea name='message'>" + textarea_content + "</textarea>"
+        submit = "<input type = 'submit'/>"
+        rotation_input = "<input type='number'name ='rotation'>"
+        form = ("<form method='post'>" +
+                rot_label + rotation_input + "<br>" +
+                message_label + textarea + "<br>"+
+                submit + "</form>")
+
+        header = "<h2>Web Caesar</h2>"
+        return header + form
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        message = "hello world!"
-        encrypted_message = ceasar.encrypt(message, 13)
-        textarea = "<textarea>" + encrypted_message + "</textarea>"
-        submit = "<input type = 'submit'/>"
-        form = "<form>"+ textarea + "<br>" + submit + "</form>"
-        self.response.write(form)
+
+        content = build_page("")
+
+        self.response.write(content)
+
+    def post(self):
+        message = self.request.get("message")
+        rotation = int(self.request.get("rotation"))
+        encrypted_message = ceasar.encrypt(message, rotation)
+        escaped_message = cgi.escape(encrypted_message)
+        content = build_page(encrypted_message)
 
 
+        self.response.write(content)
 
 
 
